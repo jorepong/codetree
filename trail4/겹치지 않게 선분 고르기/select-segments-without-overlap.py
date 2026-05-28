502842d93906b002676dcef64156f94805309322
Cho Hyunbin <79201348+jorepong@users.noreply.github.com>
2026-05-28 19:02:32 +0900

    [Passed] 겹치지 않게 선분 고르기 (49ms, 16MB)

--- trail4/겹치지 않게 선분 고르기/select-segments-without-overlap.py
+++ trail4/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -10,16 +10,13 @@ lines = []
 for i in range(n):
     lines.append((x1[i], x2[i]))
 
-lines.sort(key=lambda x: (x[1], -x[0]))
+lines.sort(key=lambda x: (x[1], x[0]))
 
-answer = 0
+answer = []
 for i in range(n):
     if i > 0 :
-        if lines[i][0] > lines[i - 1][1]:
-            answer += 1
+        if lines[i][0] > answer[-1][1]:
+            answer.append(lines[i])
     else:
-        answer += 1
-
-print(answer)
-
-# Please write your code here.
+        answer.append(lines[i])
+print(len(answer))
