3f8799b94f91ce309ef9c88ec5d257cd6c581a70
Cho Hyunbin <79201348+jorepong@users.noreply.github.com>
2026-05-30 01:13:18 +0900

    [Wrong Answer] 알파벳과 사칙연산 (91ms, 19MB)

---
+++ trail4/알파벳과 사칙연산/calculations-with-alphabet.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+
+expression = input()
+alphas = set()
+
+for i in range(len(expression)):
+    if i % 2 == 0:
+        alphas.add(expression[i])
+
+alphas = list(alphas)
+alpha_map = defaultdict(int)
+answer = 0
+
+def calculate():
+    global answer
+    result = alpha_map[expression[0]]
+    for j in range(2, len(expression), 2):
+        if expression[j-1] == '+':
+            result += alpha_map[expression[j]]
+        elif expression[j-1] == '-':
+            result -= alpha_map[expression[j]]
+        else:
+            result *= alpha_map[expression[j]]
+    answer = max(answer, result)
+
+def select(pos):
+    if pos == len(alphas):
+        calculate()
+        return
+    
+    for i in range(1, 5):
+        alpha_map[alphas[pos]] = i
+        select(pos + 1)
+
+select(0)
+print(answer)
