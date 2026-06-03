18fa295042f1fd2828d89e8560fb702461ec5d91
Cho Hyunbin <79201348+jorepong@users.noreply.github.com>
2026-06-03 23:58:00 +0900

    [Runtime Error] 피보나치 수 (82ms, 19MB)

--- trail4/피보나치 수/fibonacci-number.py
+++ trail4/피보나치 수/fibonacci-number.py
@@ -1,9 +1,15 @@
 N = int(input())
 
+memo = [-1] * (N + 1)
+memo[1] = 1
+memo[2] = 1
+
 def fib(n):
-    if n <= 2:
-        return 1
+    if memo[n] != -1:
+        return memo[n]
+    else:
+        memo[n] = fib(n-1) + fib(n-2)
     
-    return fib(n-1) + fib(n-2)
+    return memo[n]
 
 print(fib(N))
