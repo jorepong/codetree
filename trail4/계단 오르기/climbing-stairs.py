c6995179c0bbc74305004f793d059a9015ca1821
Cho Hyunbin <79201348+jorepong@users.noreply.github.com>
2026-06-04 00:04:24 +0900

    [Wrong Answer] 계단 오르기 (62ms, 17MB)

---
+++ trail4/계단 오르기/climbing-stairs.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+memo = [-1] * (n + 1)
+
+def solution(num):
+    if memo[num] != -1:
+        return memo[num]
+
+    if num == 1:
+        return 0
+    elif num == 2 or num == 3:
+        return 1
+    
+    memo[num] = solution(num - 2) + solution(num - 3)
+
+    return memo[num]
+
+print(solution(n))
