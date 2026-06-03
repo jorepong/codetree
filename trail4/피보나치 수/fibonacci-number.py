b58cfacb1b0a3e5b38e399cf4aaa782a574fe1c7
Cho Hyunbin <79201348+jorepong@users.noreply.github.com>
2026-06-03 23:59:36 +0900

    [Passed] 피보나치 수 (49ms, 16MB)

--- trail4/피보나치 수/fibonacci-number.py
+++ trail4/피보나치 수/fibonacci-number.py
@@ -1,12 +1,12 @@
 N = int(input())
 
 memo = [-1] * (N + 1)
-memo[1] = 1
-memo[2] = 1
 
 def fib(n):
     if memo[n] != -1:
         return memo[n]
+    elif n <= 2:
+        memo[n] = 1
     else:
         memo[n] = fib(n-1) + fib(n-2)
     
