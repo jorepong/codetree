9a2164c4d7f66a0ff57e0c61c2d3a60e1bcda493
Cho Hyunbin <79201348+jorepong@users.noreply.github.com>
2026-06-05 22:53:00 +0900

    [Wrong Answer] 사각형 채우기 3 (52ms, 16MB)

--- trail4/사각형 채우기 3/rectangle-fill-3.py
+++ trail4/사각형 채우기 3/rectangle-fill-3.py
@@ -2,14 +2,11 @@ n = int(input())
 
 # Please write your code here.
 
+dp = [1, 0, 0, 0]
 
-# 2 * 1 : 2가지
-# 2 * 2 : 7가지
-# 2 * 3 : dp[n-1] * 2 + dp[n-2] * 3 / 12가지 + 6가지 = 18가지 
+for _ in range(n):
+    a, b, c, d = dp
+    a, b, c, d = 2 * a + b + c + d, a + c, a + b, a
+    dp = [a, b, c, d]
 
-dp = [2, 7]
-
-for _ in range(3, n + 1):
-    dp.append(dp[-1] * 2 + dp[-2] * 3 + 2)
-
-print(dp[n-1] % 1000000007)
+print(dp[0])
