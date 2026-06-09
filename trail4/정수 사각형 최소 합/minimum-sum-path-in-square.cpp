7588a7647500f35bc0447e2a3a5e7fb18849b918
Cho Hyunbin <79201348+jorepong@users.noreply.github.com>
2026-06-09 18:05:27 +0900

    [Wrong Answer] 정수 사각형 최소 합 (2ms, 0MB)

---
+++ trail4/정수 사각형 최소 합/minimum-sum-path-in-square.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int grid[100][100];
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    // Please write your code here.
+
+    return 0;
+}
