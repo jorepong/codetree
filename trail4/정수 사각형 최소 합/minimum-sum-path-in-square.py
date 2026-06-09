bbe74bc68a94461bb677b5d5dd5839d47fd48e03
Cho Hyunbin <79201348+jorepong@users.noreply.github.com>
2026-06-09 18:05:11 +0900

    [Runtime Error] 정수 사각형 최소 합 (90ms, 18MB)

--- trail4/정수 사각형 최소 합/minimum-sum-path-in-square.py
+++ trail4/정수 사각형 최소 합/minimum-sum-path-in-square.py
@@ -1,18 +1,62 @@
-n = int(input())
-grid = [list(map(int, input().split())) for _ in range(n)]
+#include <iostream>
+#include <queue>
 
-# Please write your code here.
-dp = [[float('inf')] * n for _ in range(n)]
+using namespace std;
 
-for r in range(n):
-    for c in range(n - 1, -1, -1):
-        dp[r][c] = grid[r][c]
+typedef pair<int, int> pii;
 
-        if r > 0 and c < n - 1:
-            dp[r][c] += min(dp[r-1][c], dp[r][c+1])
-        elif r > 0:
-            dp[r][c] += dp[r-1][c]
-        elif c < n - 1:
-            dp[r][c] += dp[r][c+1]
+const int INF = 1e9;
+int n;
+int grid[100][100];
+int dp[101][101]; // dp[i][j] : (i, j)까지 갔을 때의 최소 합
+int dx[] = {1, 0};
+int dy[] = {0, -1};
+queue<pii> q;
 
-print(dp[n-1][0])
+bool isin(int x, int y) {
+    return x >= 0 && x < n && y >= 0 && y < n;
+}
+
+void bfs() {
+    q.push({0, n - 1});
+    dp[0][n - 1] = grid[0][n - 1];
+
+    while (!q.empty()) {
+        pii cur = q.front();
+        int x = cur.first;
+        int y = cur.second;
+        q.pop();
+
+        for (int i = 0; i < 2; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (isin(nx, ny)) {
+                //cout << "Debug: " << x << " " << y << " " << nx << " " << ny << "\n";
+                q.push({nx, ny});
+                dp[nx][ny] = min(dp[nx][ny], dp[x][y] + grid[nx][ny]);
+            }
+        }
+    }
+}
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    // Please write your code here.
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            dp[i][j] = INF;
+        }
+    }
+
+    bfs();
+    cout << dp[n - 1][0] << "\n";
+
+    return 0;
+}
