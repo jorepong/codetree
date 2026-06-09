277092357369a2d32cf6b920905f5595666b94e4
Cho Hyunbin <79201348+jorepong@users.noreply.github.com>
2026-06-09 18:13:08 +0900

    [Passed] 정수 사각형 최소 합 (66ms, 17MB)

--- trail4/정수 사각형 최소 합/minimum-sum-path-in-square.py
+++ trail4/정수 사각형 최소 합/minimum-sum-path-in-square.py
@@ -1,11 +1,15 @@
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
-dp = [[float('inf')] * n for _ in range(n)]
+INF = float('inf')
+dp = [[INF] * n for _ in range(n)]
+
+dp[0][n - 1] = grid[0][n - 1]
 
 for r in range(n):
     for c in range(n - 1, -1, -1):
-        dp[r][c] = grid[r][c]
+        if r == 0 and c == n - 1:
+            continue
 
         if r > 0:
             dp[r][c] = min(dp[r][c], dp[r - 1][c] + grid[r][c])
