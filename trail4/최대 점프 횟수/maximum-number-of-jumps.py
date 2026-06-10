19e298a8c4c889e4ac73691d90d46a6e2f69680b
Cho Hyunbin <79201348+jorepong@users.noreply.github.com>
2026-06-11 04:43:31 +0900

    [Wrong Answer] 최대 점프 횟수 (59ms, 17MB)

---
+++ trail4/최대 점프 횟수/maximum-number-of-jumps.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+dp = [-1] * n
+dp[0] = 0
+
+for i in range(1, n):
+    for j in range(i):
+        if j + arr[j] >= i:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(max(dp))
